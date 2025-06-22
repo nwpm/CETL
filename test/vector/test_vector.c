47d49eca115e1a4da4fedf2493164c52de572ab1
nwpm <fwxgkt@gmail.com>
2025-06-22 15:49:58 +0300

    test(vector): add core for vector tests

---
+++ test/vector/test_vector.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../../include/cstl/vector.h"
+#include "../../include/external/unity/unity.h"
+
+void setUp(){}
+void tearDown(){}
+
+int main(){
+  UNITY_BEGIN();
+  return UNITY_END();
+}
