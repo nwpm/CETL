8ac09230cd4cb3bb877b1cba86d3ace58cc1b738
nwpm <fwxgkt@gmail.com>
2025-06-23 19:26:07 +0300

    feat, style(vector): add create_empty func, and cstl prefix

--- include/cstl/vector.h
+++ include/cstl/vector.h
@@ -1,5 +1,5 @@
-#ifndef DYNAMIC_vec_H
-#define DYNAMIC_vec_H
+#ifndef CSTL_VECTOR_H
+#define CSTL_VECTOR_H
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -12,34 +12,35 @@ typedef struct cstl_vector {
   void *data;
 } cstl_vector;
 
-cstl_vector *vec_create(size_t vec_capacity, size_t elem_size);
-cstl_vector *vec_create_copy(cstl_vector *v);
-cstl_vector *vec_create_filled(size_t vec_capacity, size_t elem_size,
+cstl_vector *cstl_vec_create(size_t cstl_vec_capacity, size_t elem_size);
+cstl_vector *cstl_vec_create_copy(cstl_vector *v);
+cstl_vector *cstl_vec_create_filled(size_t cstl_vec_capacity, size_t elem_size,
                           void *init_val);
+cstl_vector *cstl_vec_create_empty();
 
-void *vec_front(cstl_vector *v);
-void *vec_back(cstl_vector *v);
-void *vec_get(cstl_vector *v, size_t index);
-void *vec_data(cstl_vector *v);
+void *cstl_vec_front(cstl_vector *v);
+void *cstl_vec_back(cstl_vector *v);
+void *cstl_vec_get(cstl_vector *v, size_t index);
+void *cstl_vec_data(cstl_vector *v);
 
-int vec_set(cstl_vector *v, size_t index, void *elem_val);
+int cstl_vec_set(cstl_vector *v, size_t index, void *elem_val);
 
-size_t vec_size(cstl_vector *v);
-size_t vec_capacity(cstl_vector *v);
+size_t cstl_vec_size(cstl_vector *v);
+size_t cstl_vec_capacity(cstl_vector *v);
 
-cstl_vector *vec_resize(cstl_vector *v, size_t new_capacity);
-cstl_vector *vec_push_back(cstl_vector *v, void *elem_val);
-cstl_vector *vec_pop_back(cstl_vector *v);
-cstl_vector *vec_shrink_to_fit(cstl_vector *v);
-cstl_vector *vec_clear(cstl_vector *v);
-cstl_vector *vec_insert(cstl_vector *v, size_t index, void *elem_val);
-cstl_vector *vec_erase(cstl_vector *v, size_t pos);
-cstl_vector *vec_insert_range(cstl_vector *v, void *range, size_t range_size,
+cstl_vector *cstl_vec_resize(cstl_vector *v, size_t new_capacity);
+cstl_vector *cstl_vec_push_back(cstl_vector *v, void *elem_val);
+cstl_vector *cstl_vec_pop_back(cstl_vector *v);
+cstl_vector *cstl_vec_shrink_to_fit(cstl_vector *v);
+cstl_vector *cstl_vec_clear(cstl_vector *v);
+cstl_vector *cstl_vec_insert(cstl_vector *v, size_t index, void *elem_val);
+cstl_vector *cstl_vec_erase(cstl_vector *v, size_t pos);
+cstl_vector *cstl_vec_insert_range(cstl_vector *v, void *range, size_t range_size,
                          size_t index);
-cstl_vector *vec_erase_range(cstl_vector *v, size_t index, size_t len);
+cstl_vector *cstl_vec_erase_range(cstl_vector *v, size_t index, size_t len);
 
-bool vec_is_empty(cstl_vector *v);
+bool cstl_vec_is_empty(cstl_vector *v);
 
-void vec_free(cstl_vector *v);
+void cstl_vec_free(cstl_vector *v);
 
-#endif // DYNAMIC_vec_H
+#endif // CSTL_VECTOR_H
