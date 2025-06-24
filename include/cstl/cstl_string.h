d0ca147cfeffc70522c4ccc76e02d406e85e7d16
nwpm <fwxgkt@gmail.com>
2025-06-24 19:30:49 +0300

    feat(string): add string container and part functions for str
    string.h
    Add declarations:
    -create
    -create_empty
    -create_filled
    -create_from_cstr
    -resize
    -append
    -append_char
    -length
    -capacity
    -data
    -is_empty
    -free

---
+++ include/cstl/cstl_string.h
@@ -0,0 +1,37 @@
+#ifndef CSTL_STRING_H
+#define CSTL_STRING_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct cstl_string {
+
+  char *data;
+  size_t length;
+  size_t capacity;
+
+} cstl_string;
+
+cstl_string *cstl_str_create_empty();
+cstl_string *cstl_str_create(size_t len);
+cstl_string *cstl_str_create_filled(size_t len, int c);
+cstl_string *cstl_str_create_from_cstr(const char *str);
+cstl_string *cstl_str_create_copy(cstl_string *s);
+
+cstl_string *cstl_str_resize(cstl_string *s, size_t new_len);
+cstl_string *cstl_str_append(cstl_string *s, const char *c_s);
+cstl_string *cstl_str_append_char(cstl_string *s, int c);
+cstl_string *cstl_str_insert(cstl_string *s, size_t pos, const char* c_s);
+cstl_string *cstl_str_erase(cstl_string *s, size_t pos, size_t len);
+cstl_string *cstl_str_clear(cstl_string *s);
+
+size_t cstl_str_length(cstl_string *s);
+size_t cstl_str_capacity(cstl_string *s);
+
+char *cstl_str_data(cstl_string *s);
+
+bool cstl_str_is_empty(cstl_string *s);
+
+void cstl_str_free(cstl_string *s);
+
+#endif // CSTL_STRING_H
