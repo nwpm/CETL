83c11eb2448d843291aa84270e2d778ae5907f63
nwpm <fwxgkt@gmail.com>
2025-06-28 16:06:04 +0300

    test(list): add list test file

---
+++ test/list/test_list.c
@@ -0,0 +1,13 @@
+#include "../../include/cstl/cstl_list.h"
+#include "../../include/external/unity/unity.h"
+#include "../../include/external/unity/unity_internals.h"
+
+void setUp(){}
+void tearDown(){}
+
+int main(){
+
+  UNITY_BEGIN();
+
+  return UNITY_END();
+}
