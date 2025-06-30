23b1bc4d5525e37e42e551433e8d2d2a7fd9f082
nwpm <fwxgkt@gmail.com>
2025-06-30 19:41:16 +0300

    feat, refactor(stack): add new funcs, refactor old version
    Add:
    -create_copy
    -clear
    -is_empty
    
    Refactor:
    -rebuild stack for new version of list

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,98 +1,123 @@
-#include "stack.h"
+#include "../../include/cstl/cstl_stack.h"
+#include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 
-stack *stack_create(void) {
+cstl_stack *cstl_stack_create_empty() {
 
-  stack *s = (stack *)malloc(sizeof(stack));
-
-  if (s == NULL) {
-    return NULL;
-  }
-
-  list *l = (list *)malloc(sizeof(list));
-
-  if (l == NULL) {
-    return NULL;
-  }
-
-  list_create(l);
-
-  s->data_ptr = l;
-  s->size = l->size;
-
-  return s;
-}
-
-stack *stack_create_init(int32_t value) {
-
-  stack *s = (stack *)malloc(sizeof(stack));
+  cstl_stack *s = malloc(sizeof(cstl_stack));
 
   if (s == NULL) {
     return NULL;
   }
 
-  list *l = (list *)malloc(sizeof(list));
+  cstl_list *l = cstl_list_create_empty();
 
   if (l == NULL) {
     return NULL;
   }
 
-  list_create_init(l, value);
-
-  s->data_ptr = l;
-  s->size = l->size;
+  s->size = 0;
+  s->data = l;
 
   return s;
 }
 
-stack *stack_push(stack *s, int32_t value) {
+cstl_stack *cstl_stack_create(void *data, size_t elem_size) {
 
-  list_push_back(s->data_ptr, value);
-  s->size = s->data_ptr->size;
-
-  return s;
-}
-
-stack *stack_pop(stack *s) {
-
-  if (s->size == 0) {
+  if (data == NULL) {
     return NULL;
   }
 
-  list_pop_back(s->data_ptr);
-  s->size = s->data_ptr->size;
+  cstl_stack *s = malloc(sizeof(cstl_stack));
+
+  if (s == NULL) {
+    return NULL;
+  }
+
+  cstl_list *l = cstl_list_create(data, elem_size);
+
+  if (l == NULL) {
+    return NULL;
+  }
+
+  s->size = 1;
+  s->data = l;
 
   return s;
 }
 
-int32_t stack_top(stack *s) {
-  if (s->size == 0) {
-    return -1; //???
+cstl_stack *cstl_stack_create_copy(cstl_stack *s) {
+
+  if (s == NULL) {
+    return NULL;
   }
-  return s->data_ptr->tail->data;
+
+  cstl_stack *res = cstl_stack_create_empty();
+
+  _cstl_node *current = s->data->head;
+  size_t elem_size = s->data->elem_size;
+
+  while(current){
+    cstl_list_push_back(res->data, current->data, elem_size);
+    current = current->next;
+  }
+
+  return s;
 }
 
-void stack_print_data(stack *s) { list_print_data(s->data_ptr); }
+cstl_stack *cstl_stack_push(cstl_stack *s, void *data, size_t elem_size) {
 
-void stack_free(stack *s) {
+  if (s == NULL || data == NULL) {
+    return NULL;
+  }
 
-  list_free_nodes(s->data_ptr);
-  free(s->data_ptr);
+  cstl_list_push_back(s->data, data, elem_size);
+  s->size++;
+
+  return s;
+}
+
+cstl_stack *cstl_stack_pop(cstl_stack *s) {
+
+  if (s == NULL || s->size == 0) {
+    return NULL;
+  }
+
+  cstl_list_pop_back(s->data);
+  s->size--;
+
+  return s;
+}
+
+bool cstl_stack_is_empty(cstl_stack *s){
+  return cstl_list_is_empty(s->data);
+}
+
+void *cstl_stack_top(cstl_stack *s) {
+
+  if (s == NULL || s->size == 0) {
+    return NULL;
+  }
+
+  return s->data->tail->data;
+}
+
+void cstl_stack_clear(cstl_stack *s){
+
+  if(s == NULL){
+    return;
+  }
+
+  cstl_list_clear(s->data);
+  s->size = 0;
+}
+
+void cstl_stack_free(cstl_stack *s) {
+
+  if (s == NULL) {
+    return;
+  }
+
+  cstl_list_free(s->data);
   free(s);
-
-}
-
-int main(void) {
-
-  stack *s = stack_create_init(2);
-
-  stack_push(s, 3);
-  stack_pop(s);
-
-  stack_print_data(s);
-
-  stack_free(s);
-
-  return 0;
 }
