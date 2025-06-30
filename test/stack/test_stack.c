c7b8ef103abfbda71b2f81ada1ae1a5c41e86139
nwpm <fwxgkt@gmail.com>
2025-06-30 19:37:38 +0300

    test(stack): add empty test file for stack

---
+++ test/stack/test_stack.c
@@ -0,0 +1,11 @@
+#include "../../include/cstl/cstl_stack.h"
+#include "../../include/external/unity/unity.h"
+#include "../../include/external/unity/unity_internals.h"
+
+void setUp(){}
+void tearDown(){}
+
+int main(){
+  UNITY_BEGIN();
+  return UNITY_END();
+}
