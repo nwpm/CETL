b6528013bb7bf12300fc67ba29b355a59c24a902
nwpm <fwxgkt@gmail.com>
2025-07-03 09:20:53 +0300

    fix, refactor(queue): fixes for queue
    Remove:
    -queue_push_range
    
    Fix:
    -push, pop: change size now
    
    Refactor:
    -pop: delete param data and elem_size

--- include/cstl/cstl_queue.h
+++ include/cstl/cstl_queue.h
@@ -2,8 +2,8 @@
 #define CSTL_QUEUE_H
 
 #include "../cstl/cstl_llist.h"
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef struct cstl_cstl_queue {
 
@@ -17,15 +17,14 @@ cstl_queue *cstl_queue_create(void *data, size_t elem_size);
 cstl_queue *cstl_queue_create_copy(cstl_queue *q);
 
 cstl_queue *cstl_queue_push(cstl_queue *q, void *data, size_t elem_size);
-cstl_queue *cstl_queue_push_range(cstl_queue *q, void *data, size_t elem_size, size_t pos);
-cstl_queue *cstl_queue_pop(cstl_queue *q, void *data, size_t elem_size);
+cstl_queue *cstl_queue_pop(cstl_queue *q);
 
 size_t cstl_queue_size(cstl_queue *q);
 
 bool cstl_queue_is_empty(cstl_queue *q);
 
-void* cstl_queue_front(cstl_queue *q);
-void* cstl_queue_back(cstl_queue *q);
+void *cstl_queue_front(cstl_queue *q);
+void *cstl_queue_back(cstl_queue *q);
 
 // cstl_queue_swap
 void cstl_queue_free(cstl_queue *q);
--- src/queue/queue.c
+++ src/queue/queue.c
@@ -1,16 +1,17 @@
 #include "../../include/cstl/cstl_queue.h"
+#include <stddef.h>
 
-cstl_queue* cstl_queue_create_empty(){
+cstl_queue *cstl_queue_create_empty() {
 
-  cstl_queue* q = malloc(sizeof(cstl_queue));
-  
-  if(q == NULL){
+  cstl_queue *q = malloc(sizeof(cstl_queue));
+
+  if (q == NULL) {
     return NULL;
   }
 
   cstl_llist *llist = cstl_llist_create_empty();
 
-  if(llist == NULL){
+  if (llist == NULL) {
     return NULL;
   }
 
@@ -20,15 +21,15 @@ cstl_queue* cstl_queue_create_empty(){
   return q;
 }
 
-cstl_queue *cstl_queue_create(void *data, size_t elem_size){
+cstl_queue *cstl_queue_create(void *data, size_t elem_size) {
 
-  if(data == NULL || elem_size == 0){
+  if (data == NULL || elem_size == 0) {
     return NULL;
   }
 
   cstl_queue *q = cstl_queue_create_empty();
 
-  if(q == NULL){
+  if (q == NULL) {
     return NULL;
   }
 
@@ -39,87 +40,82 @@ cstl_queue *cstl_queue_create(void *data, size_t elem_size){
   return q;
 }
 
-cstl_queue *cstl_queue_create_copy(cstl_queue *q){
+cstl_queue *cstl_queue_create_copy(cstl_queue *q) {
 
-  if(q == NULL){
+  if (q == NULL) {
     return NULL;
   }
 
   cstl_queue *c_q = cstl_queue_create_empty();
 
-  if(c_q == NULL){
+  if (c_q == NULL) {
     return NULL;
   }
 
-  c_q->size = q->size;
-
   _cstl_node *current = q->data->head;
-  
-  while(current){
+
+  while (current) {
     cstl_queue *res = cstl_queue_push(c_q, current->data, q->data->elem_size);
     current = current->next;
 
-    if(res == NULL){
+    if (res == NULL) {
       cstl_queue_free(c_q);
       return NULL;
     }
   }
-  
+
   return c_q;
 }
 
-void *cstl_queue_front(cstl_queue *q){
-  
-  if(q == NULL || q->size == 0){
+void *cstl_queue_front(cstl_queue *q) {
+
+  if (q == NULL || q->size == 0) {
     return NULL;
   }
 
   return cstl_llist_get(q->data, 0);
 }
 
-void *cstl_queue_back(cstl_queue *q){
+void *cstl_queue_back(cstl_queue *q) {
 
-  if(q == NULL || q->size == 0){
+  if (q == NULL || q->size == 0) {
     return NULL;
   }
 
   return cstl_llist_get(q->data, q->size - 1);
 }
 
-cstl_queue *cstl_queue_push(cstl_queue *q, void *data, size_t elem_size){
-  
-  if(q == NULL || data == NULL || elem_size == 0){
+cstl_queue *cstl_queue_push(cstl_queue *q, void *data, size_t elem_size) {
+
+  if (q == NULL || data == NULL || elem_size == 0) {
     return NULL;
   }
 
   cstl_llist_push_back(q->data, data, elem_size);
+  q->size++;
 
   return q;
 }
 
-cstl_queue *cstl_queue_pop(cstl_queue *q, void *data, size_t elem_size){
-  
-  if(q == NULL || data == NULL || elem_size == 0){
+cstl_queue *cstl_queue_pop(cstl_queue *q) {
+
+  if (q == NULL) {
     return NULL;
   }
 
   cstl_llist_pop_front(q->data);
+  q->size--;
 
   return q;
 }
 
-size_t cstl_queue_size(cstl_queue *q){
-  return q->size;
-}
+size_t cstl_queue_size(cstl_queue *q) { return q->size; }
 
-bool cstl_queue_is_empty(cstl_queue *q){
-  return q && !q->size;
-}
+bool cstl_queue_is_empty(cstl_queue *q) { return q && !q->size; }
 
-void cstl_queue_free(cstl_queue *q){
+void cstl_queue_free(cstl_queue *q) {
 
   cstl_llist_free_nodes(q->data);
   free(q->data);
   free(q);
-
 }
