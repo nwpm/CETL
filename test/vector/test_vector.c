9e3723c8ce3a79f8a0a9407e395932b69541b38f
nwpm <fwxgkt@gmail.com>
2025-09-12 19:55:26 +0300

    test(vec): add tests for 'resize'

--- test/vector/test_vector.c
+++ test/vector/test_vector.c
@@ -24,6 +24,9 @@ typedef struct TestHeapStruct {
   cetl_str_t data;
 } TestHeapStruct;
 
+typedef cetl_void_t (*vec_filler)(cetl_vector *vec, cetl_size_t size);
+typedef cetl_void_t (*vec_checker)(const cetl_vector *vec, cetl_size_t size);
+
 // ********** Base types **********
 cetl_element *create_int_type() {
 
@@ -147,8 +150,8 @@ cetl_void_t fill_char_vector(cetl_vector *vec, cetl_size_t size) {
 }
 
 cetl_void_t fill_struct_vector(cetl_vector *vec, cetl_size_t size) {
-  
-  TestStruct init_value = {1, 2}; 
+
+  TestStruct init_value = {1, 2};
 
   for (cetl_size_t i = 0; i < size; ++i) {
     cetl_vec_push_back(vec, &init_value);
@@ -156,8 +159,8 @@ cetl_void_t fill_struct_vector(cetl_vector *vec, cetl_size_t size) {
 }
 
 cetl_void_t fill_heap_struct_vector(cetl_vector *vec, cetl_size_t size) {
-  
-  TestHeapStruct *init_value = malloc(sizeof(TestHeapStruct)); 
+
+  TestHeapStruct *init_value = malloc(sizeof(TestHeapStruct));
   init_value->data = "string";
 
   for (cetl_size_t i = 0; i < size; ++i) {
@@ -168,6 +171,70 @@ cetl_void_t fill_heap_struct_vector(cetl_vector *vec, cetl_size_t size) {
 }
 
 // ********************
+
+// ********** Data checker **********
+
+cetl_void_t int_vector_checker(const cetl_vector *vec, cetl_size_t size) {
+
+  cetl_int_t init_value = 1;
+
+  for (cetl_size_t i = 0; i < size; ++i) {
+    TEST_ASSERT_EQUAL_INT(init_value, *((int *)cetl_vec_get(vec, i)));
+  }
+}
+
+cetl_void_t double_vector_checker(const cetl_vector *vec, cetl_size_t size) {
+
+  cetl_double_t init_value = 1.;
+
+  for (cetl_size_t i = 0; i < size; ++i) {
+    TEST_ASSERT_EQUAL_INT(init_value, *((double *)cetl_vec_get(vec, i)));
+  }
+}
+
+cetl_void_t char_vector_checker(const cetl_vector *vec, cetl_size_t size) {
+
+  cetl_char_t init_value = 1;
+
+  for (cetl_size_t i = 0; i < size; ++i) {
+    TEST_ASSERT_EQUAL_CHAR(init_value, *((char *)cetl_vec_get(vec, i)));
+  }
+}
+
+cetl_void_t struct_vector_checker(const cetl_vector *vec, cetl_size_t size) {
+
+  TestStruct init_value = {1, 2};
+
+  for (cetl_size_t i = 0; i < size; ++i) {
+    TEST_ASSERT_EQUAL_INT(init_value.x,
+                          ((TestStruct *)cetl_vec_get(vec, i))->x);
+    TEST_ASSERT_EQUAL_INT(init_value.y,
+                          ((TestStruct *)cetl_vec_get(vec, i))->y);
+  }
+}
+
+cetl_void_t heap_struct_vector_checker(const cetl_vector *vec,
+                                       cetl_size_t size) {
+
+  TestHeapStruct *init_value = malloc(sizeof(TestHeapStruct));
+  init_value->data = "string";
+
+  for (cetl_size_t i = 0; i < size; ++i) {
+    TEST_ASSERT_EQUAL_STRING(init_value->data,
+                             ((TestHeapStruct *)cetl_vec_get(vec, i))->data);
+  }
+
+  free(init_value);
+}
+
+cetl_void_t empty_data_checker(const cetl_vector *vec, cetl_size_t size) {
+
+  TEST_ASSERT_EQUAL_size_t(0, size);
+  TEST_ASSERT_NULL(cetl_vec_data(vec));
+}
+
+// ********************
+
 void setUp() {
 
   int_type = create_int_type();
@@ -217,9 +284,8 @@ cetl_void_t test_create_empty_type_heap_struct() {
 
 // ********** Create copy **********
 
-typedef cetl_void_t (*vec_filler)(cetl_vector *vec, cetl_size_t size);
-
-cetl_void_t check_copy(const cetl_element *type, vec_filler filler, cetl_size_t size) {
+cetl_void_t check_copy(const cetl_element *type, vec_filler filler,
+                       cetl_size_t size) {
 
   cetl_vector *source = cetl_vec_create_empty(type);
   TEST_ASSERT_NOT_NULL(source);
@@ -278,6 +344,128 @@ cetl_void_t test_create_copy_type_heap_struct_size_100() {
 }
 
 //
+
+// ********** Resize **********
+
+cetl_void_t check_resize(const cetl_element *type, vec_filler filler,
+                         cetl_size_t start_size, cetl_size_t new_capacity,
+                         vec_checker checker) {
+
+  cetl_vector *vec = cetl_vec_create_empty(type);
+  TEST_ASSERT_NOT_NULL(vec);
+
+  filler(vec, start_size);
+
+  TEST_ASSERT_NOT_NULL(cetl_vec_resize(vec, new_capacity));
+  TEST_ASSERT_EQUAL_size_t(new_capacity, cetl_vec_capacity(vec));
+
+  checker(vec, cetl_vec_size(vec));
+
+  cetl_vec_free(vec);
+}
+
+// Int
+cetl_void_t test_resize_type_int_start_size_0_new_capacity_0() {
+  check_resize(int_type, fill_int_vector, 0, 0, empty_data_checker);
+}
+
+cetl_void_t test_resize_type_int_start_size_10_new_capacity_5() {
+  check_resize(int_type, fill_int_vector, 10, 5, int_vector_checker);
+}
+
+cetl_void_t test_resize_type_int_start_size_10_new_capacity_10() {
+  check_resize(int_type, fill_int_vector, 10, 10, int_vector_checker);
+}
+
+cetl_void_t test_resize_type_int_start_size_10_new_capacity_15() {
+  check_resize(int_type, fill_int_vector, 10, 15, int_vector_checker);
+}
+
+cetl_void_t test_resize_type_int_start_size_1000_new_capacity_250() {
+  check_resize(int_type, fill_int_vector, 1000, 250, int_vector_checker);
+}
+
+cetl_void_t test_resize_type_int_start_size_1000_new_capacity_2000() {
+  check_resize(int_type, fill_int_vector, 1000, 2000, int_vector_checker);
+}
+
+// Double
+cetl_void_t test_resize_type_double_start_size_10_new_capacity_5() {
+  check_resize(double_type, fill_double_vector, 10, 5, double_vector_checker);
+}
+
+cetl_void_t test_resize_type_double_start_size_10_new_capacity_10() {
+  check_resize(double_type, fill_double_vector, 10, 10, double_vector_checker);
+}
+
+cetl_void_t test_resize_type_double_start_size_10_new_capacity_15() {
+  check_resize(double_type, fill_double_vector, 10, 15, double_vector_checker);
+}
+
+cetl_void_t test_resize_type_double_start_size_1000_new_capacity_250() {
+  check_resize(double_type, fill_double_vector, 1000, 250,
+               double_vector_checker);
+}
+
+cetl_void_t test_resize_type_double_start_size_1000_new_capacity_2000() {
+  check_resize(double_type, fill_double_vector, 1000, 2000,
+               double_vector_checker);
+}
+
+// Struct
+cetl_void_t test_resize_type_struct_start_size_10_new_capacity_5() {
+  check_resize(struct_type, fill_struct_vector, 10, 5, struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_struct_start_size_10_new_capacity_10() {
+  check_resize(struct_type, fill_struct_vector, 10, 10, struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_struct_start_size_10_new_capacity_15() {
+  check_resize(struct_type, fill_struct_vector, 10, 15, struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_struct_start_size_1000_new_capacity_250() {
+  check_resize(struct_type, fill_struct_vector, 1000, 250,
+               struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_struct_start_size_1000_new_capacity_2000() {
+  check_resize(struct_type, fill_struct_vector, 1000, 2000,
+               struct_vector_checker);
+}
+
+// Heap Struct
+cetl_void_t test_resize_type_heap_struct_start_size_10_new_capacity_5() {
+  check_resize(heap_struct_type, fill_heap_struct_vector, 10, 5,
+               heap_struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_heap_struct_start_size_10_new_capacity_10() {
+  check_resize(heap_struct_type, fill_heap_struct_vector, 10, 10,
+               heap_struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_heap_struct_start_size_10_new_capacity_15() {
+  check_resize(heap_struct_type, fill_heap_struct_vector, 10, 15,
+               heap_struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_heap_struct_start_size_1000_new_capacity_250() {
+  check_resize(heap_struct_type, fill_heap_struct_vector, 1000, 250,
+               heap_struct_vector_checker);
+}
+
+cetl_void_t test_resize_type_heap_struct_start_size_1000_new_capacity_2000() {
+  check_resize(heap_struct_type, fill_heap_struct_vector, 1000, 2000,
+               heap_struct_vector_checker);
+}
+
+// ********************
+
+// ********** Push Back **********
+// ********************
+
 int main() {
 
   UNITY_BEGIN();
@@ -303,6 +491,33 @@ int main() {
   RUN_TEST(test_create_copy_type_char_size_100);
   RUN_TEST(test_create_copy_type_struct_size_100);
   RUN_TEST(test_create_copy_type_heap_struct_size_100);
+
+  puts("\nResize\n");
+
+  RUN_TEST(test_resize_type_int_start_size_0_new_capacity_0);
+  RUN_TEST(test_resize_type_int_start_size_10_new_capacity_5);
+  RUN_TEST(test_resize_type_int_start_size_10_new_capacity_10);
+  RUN_TEST(test_resize_type_int_start_size_10_new_capacity_15);
+  RUN_TEST(test_resize_type_int_start_size_1000_new_capacity_250);
+  RUN_TEST(test_resize_type_int_start_size_1000_new_capacity_2000);
+
+  RUN_TEST(test_resize_type_double_start_size_10_new_capacity_5);
+  RUN_TEST(test_resize_type_double_start_size_10_new_capacity_10);
+  RUN_TEST(test_resize_type_double_start_size_10_new_capacity_15);
+  RUN_TEST(test_resize_type_double_start_size_1000_new_capacity_250);
+  RUN_TEST(test_resize_type_double_start_size_1000_new_capacity_2000);
+
+  RUN_TEST(test_resize_type_struct_start_size_10_new_capacity_5);
+  RUN_TEST(test_resize_type_struct_start_size_10_new_capacity_10);
+  RUN_TEST(test_resize_type_struct_start_size_10_new_capacity_15);
+  RUN_TEST(test_resize_type_struct_start_size_1000_new_capacity_250);
+  RUN_TEST(test_resize_type_struct_start_size_1000_new_capacity_2000);
+
+  RUN_TEST(test_resize_type_heap_struct_start_size_10_new_capacity_5);
+  RUN_TEST(test_resize_type_heap_struct_start_size_10_new_capacity_10);
+  RUN_TEST(test_resize_type_heap_struct_start_size_10_new_capacity_15);
+  RUN_TEST(test_resize_type_heap_struct_start_size_1000_new_capacity_250);
+  RUN_TEST(test_resize_type_heap_struct_start_size_1000_new_capacity_2000);
 
   return UNITY_END();
 }
